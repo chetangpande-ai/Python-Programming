afd4cec18c94903cc541362ce29ec290c0eabccd
seleniumtrainer2012 <seleniumtrainer2012@gmail.com>
2025-09-09 10:21:24 +0530

    added examples on inheritance, super concepts

---
+++ 5-OOPs-2-packgaes/dummy.py
@@ -0,0 +1,10 @@
+def calc():
+    print("calc")
+
+
+class A():
+    def __init__(self):
+        pass
+
+    def calc(self):
+        print("A-Calc")
---
+++ 5-OOPs-2-packgaes/test.py
@@ -0,0 +1,5 @@
+from dummy import calc,A
+
+calc()
+a1=A()
+a1.calc()
