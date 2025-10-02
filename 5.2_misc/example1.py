a02135fd234c5487248bd333a26184949bfecd6a
chetangpande-ai <chetangpande1@gmail.com>
2025-10-02 09:45:35 +0530

    added __name__

---
+++ 5.2_misc/example1.py
@@ -0,0 +1,6 @@
+def greet():
+    print("Hello, World!")
+
+if __name__ == "__main__":
+    print("value of __name__  is -",__name__)
+    greet()
---
+++ 5.2_misc/example2.py
@@ -0,0 +1,4 @@
+from example1 import greet
+
+print(__name__)
+greet()
