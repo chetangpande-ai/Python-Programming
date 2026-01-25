a6f93d7c780c7344f92d58679547da50512249e7
chetangpande-ai <chetangpande1@gmail.com>
2026-01-24 23:48:25 -0800

    time n space complexicity

---
+++ 24-01-2026/example1.py
@@ -0,0 +1,28 @@
+def merge_sort(arr):
+    if len(arr) <= 1:
+        return arr
+
+    mid = len(arr) // 2
+    left = merge_sort(arr[:mid])
+    right = merge_sort(arr[mid:])
+
+    return merge(left, right)
+
+def merge(left, right):
+    result = []
+    i = j = 0
+    while i < len(left) and j < len(right):
+        if left[i] < right[j]:
+            result.append(left[i])
+            i += 1
+        else:
+            result.append(right[j])
+            j += 1
+    result.extend(left[i:])
+    result.extend(right[j:])
+    return result
+
+if __name__ == "__main__":
+    myarray = [456,12,78,76,12,45,234,123,67,89,0,45,34,7563,24,8756,2345]
+    sorted_myarray = merge_sort(myarray)
+    print(sorted_myarray)
